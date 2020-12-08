33f189bd5ebf871e54dd23ea6c65fcd112737d3e
liameno16 <liameno16@gmail.com>
2020-12-08 14:50:58 -0800

    finished permutations with two different solutions, the first using binary trees starting with each index, and a second using one tree where the current value is added at each index of a number of permutations

--- permutations.js
+++ permutations.js
@@ -1,39 +1,65 @@
 /* GTCI Subsets 3: Given an array of distinct numbers, find all of its permutations */
-
+// O(n*n!) time, O(n*n!) space
 const permutations = function(nums) {
   let result = [];
   // Iterate over nums from index 1 to end
   for (let i = 0; i < nums.length; i += 1) {
     let currentPerm = [nums[i]];
-    // Get slice of array not including current element
+    // Get slice of array which will later not including current element
     let slice = [...nums];
-    slice.splice(i, 1);
-    console.log(slice)
-    // Trigger helper function with current perm array and slice
-    currentPerm = helper(currentPerm, slice, []);
-    // Concat the new permutations to the result array
-    result = result.concat(currentPerm);
+    // Trigger helper function with the current permutation and get all permutations starting with the current value
+    helper(currentPerm, slice, i, result, nums.length);
   }
   return result;
 }
 
-// Helper function that takes an array and slice of array to iterate over and returns array of array
-const helper = function(arr, slice, result) {
-  // IF slice has no elements, return
-  if (!slice.length) return result;
-  // Iterate over slice and push those elements to copy of arr
-  slice.forEach(val => {
-    // Create temp array to push slice val to
-    const temp = [...arr];
-    temp.push(val);
-    // Push to result
-    result.push(temp);
-  });
-  console.log(result)
-  // Invoke helper again with slice and last value taken out
-  return helper(arr, slice.slice(0, slice.length - 1), result);
+// Helper function that takes an array which holds results and slice of array to iterate over and just modifies result
+const helper = function(currentArr, slice, index, result, numLength) {
+  // Get slice of all array elements not including index
+  slice.splice(index, 1);
+  // Base case if the slice now has no values, return array of arrays
+  if (!slice.length) return;
+  // Else iterate over elements in slice 
+  for (let i = 0; i < slice.length; i += 1) {
+    // Make shallow copy of current array
+    const shallowCopy = [...currentArr];
+    // Push the current slice value to the copy
+    shallowCopy.push(slice[i]);
+    // Invoke the helper with the copy as the current array to get permutations starting from this copy
+    helper(shallowCopy, [...slice], i, result, numLength);
+    // Concatenate the new permutations to result if the shallow copy matches the length of nums
+    if (shallowCopy.length === numLength) result.push(shallowCopy);
+  }
+}
+
+// Alternative where i insert current value at every possible index 
+function find_permutations(nums) {
+  // Result holds final array of permutations
+  const result = [];
+  // Permutations holds interim arrays of permutations
+  const permutations = [[]];
+  // Iterate over the nums array
+  for (let i = 0; i < nums.length; i += 1) {
+    // Iterate over the list of permutations
+    const n = permutations.length;
+    for (let j = 0; j < n; j += 1) {
+      // Get the first element of the permutations array and remove it
+      const oldPermutation = permutations.shift();
+      // Create a new permutation by adding the current value to every possible index
+      for (let p = 0; p < oldPermutation.length + 1; p += 1) {
+        // Create clone of old permutation
+        const newPermutation = [...oldPermutation];
+        // Insert the current number at index p
+        newPermutation.splice(p, 0, nums[i]);
+        // If the new permutation is the same length as nums, push it to result, else push it to permutations
+        newPermutation.length === nums.length ? result.push(newPermutation) : permutations.push(newPermutation);
+      }
+    }
+  }
+  // Return the result
+  return result;
 }
 
 // Tests: 
 const arr1 = [1, 3, 5];
-console.log(permutations(arr1));
+console.log(find_permutations(arr1));
