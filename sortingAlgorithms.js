462b1ed783857310bd9deea9222c8d436ded3ab4
liameno16 <liameno16@gmail.com>
2020-12-11 15:11:27 -0800

    finished merge sort

--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -66,3 +66,38 @@ const insertionSort = function(arr) {
 // Tests:
 const arr3 = [5, 1, 4, 2, 8];
 console.log(insertionSort(arr3));
+
+// Merge Sort: Divide input array into smaller and smaller pieces then insert elements in correct order - O(nlogn) time, O(n) space
+const mergeSort = function(arr) {
+  // If the array has one elemnt or empty, return it
+  if (arr.length <= 1) return arr;
+  // Find the midpoint
+  const midpoint = Math.floor(arr.length / 2);
+  // Split array into two halves
+  const left = arr.slice(0, midpoint);
+  const right = arr.slice(midpoint);
+  // Invoke merge helper function 
+  return merge(mergeSort(left), mergeSort(right));
+}
+// Function that merges array elements in correct order (assumes theyre both sorted)
+const merge = function(left, right) {
+  // Initialize array to hold sorted elements
+  let sorted = [];
+  // While either left or right have, elements, insert smaller into sorted
+  while(left[0] && right[0]) {
+    if (left[0] < right[0]) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+  // For remaining elements, concat them to the sorted arr and return it
+  if (left.length) sorted = sorted.concat(left);
+  if (right.length) sorted = sorted.concat(right);
+  return sorted;
+}
+
+// Tests:
+const arr4 = [5, 1, 4, 2, 8];
+const arr5 = [1, 6, 1, 4, 23, 5, 9, 23];
+console.log(mergeSort(arr5));
