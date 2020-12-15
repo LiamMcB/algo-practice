c3d69f38fcce936b72b2a4a3c153b1010005ef33
liameno16 <liameno16@gmail.com>
2020-12-14 16:52:09 -0800

    did additional practice on subsets

--- mergeIntervalsRedo.js
+++ mergeIntervalsRedo.js
@@ -30,3 +30,21 @@ const mergeIntervals = function(intervals) {
 // Tests: 
 const arr1 = [[6,7], [2,4], [5,9]];
 console.log(mergeIntervals(arr1));
+
+// Given an array of unique values, return an array of all distinct subsets
+const distinctSubsets = function(arr) {
+  let subsets = [[]];
+  for (let i = 0; i < arr.length; i += 1) {
+    // Create copy of subsets to push into
+    const copy = [...subsets];
+    for (let j = 0; j < copy.length; j += 1) {
+      copy[j].push(arr[i]);
+    }
+    subsets = subsets.concat(copy);
+  }
+  return subsets;
+}
+
+// Tests:
+const arr2 = [1, 3];
+console.log(distinctSubsets(arr2));
