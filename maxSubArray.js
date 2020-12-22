4ba3a6967d00da60b54a234dd39aa8240fd8d43d
liameno16 <liameno16@gmail.com>
2020-12-22 11:46:39 -0800

    finished maxSubArray of any size without sliding window

--- maxSubArray.js
+++ maxSubArray.js
@@ -29,3 +29,40 @@ function maxSubArray(arr, k) {
 // Test Case:
 const arr = [1, 4, 2, 0, 8, 1, 3];
 console.log(maxSubArray(arr, 3));
+
+// Solution with no size k, just finding the max subarray of any length and return the sum
+const maxSubArrayofAnyLength = (arr) => {
+  // O(n^2) time O(1) space
+  // // Initialize sum variable to hold max sum
+  // let maxSum = arr[0];
+  // // Iterate over the array
+  // for (let i = 0; i < arr.length; i += 1) {
+  //   let currentSum = arr[i];
+  //   let index = i - 1;
+  //   // While loop until moving back from current index to start
+  //   while (index >= 0) {
+  //     currentSum += arr[index];
+  //     maxSum = Math.max(maxSum, currentSum);
+  //     index -= 1;
+  //   }
+  // }
+  // // Return the maximum sum
+  // return maxSum;
+  
+  // O(n) time, O(1) space
+  // Declare a previous sum and set to zero and max to -Inf
+  let prevSum = 0;
+  let maxSum = -Infinity;
+  // Iterate over array
+  for (var i = 0; i < arr.length; i++) {
+    // If current arr element is positive, reset previous sum to add it, else add it as long as adding it doesnt make it smaller than the current el
+    prevSum = Math.max(prevSum + arr[i], arr[i]);
+    // Set max sum to be the maximum of the current max sum and the previous
+    maxSum = Math.max(maxSum, prevSum);
+  }
+  return maxSum;
+}
+// Test Cases:
+const arr2 = [-2,1,-3,4,-1,2,1,-5,4];
+const arr3 = [-1, 4, -1, 2, 0]
+console.log(maxSubArrayofAnyLength(arr3));
