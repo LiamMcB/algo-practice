676c76cd55f12c4b2ce43208b95058a2e7df4360
liameno16 <liameno16@gmail.com>
2021-01-05 10:13:19 -0800

    did additional practice with shortest path of weighted nodes in graph

--- graphReview.js
+++ graphReview.js
@@ -5,17 +5,17 @@ class Node {
     this.adjList = [];
   }
 }
-const root = new Node('A');
-const bNode = new Node('B');
-const cNode = new Node('C');
-const dNode = new Node('D');
-const eNode = new Node('E');
-root.adjList.push(bNode);
-root.adjList.push(cNode);
-bNode.adjList.push(dNode);
-cNode.adjList.push(dNode);
-dNode.adjList.push(eNode);
-console.log(root); // A -> B, C -> D -> E 
+// const root = new Node('A');
+// const bNode = new Node('B');
+// const cNode = new Node('C');
+// const dNode = new Node('D');
+// const eNode = new Node('E');
+// root.adjList.push(bNode);
+// root.adjList.push(cNode);
+// bNode.adjList.push(dNode);
+// cNode.adjList.push(dNode);
+// dNode.adjList.push(eNode);
+// console.log(root); // A -> B, C -> D -> E 
 
 /* Find Path: Given two node, see if there is a bath between nodes with those values */
 const findPathDFS = function(root, target) {
@@ -49,4 +49,40 @@ const findPathBFS = function(root, target) {
 }
 
 // Tests:
-console.log(findPathBFS(root, eNode))
+// console.log(findPathBFS(root, eNode));
+
+/* Shortest Path: Find the path with the smallest sum between two nodes in a graph and return that sum, if it exists 
+(I made this up, all nodes are positive) */
+const shortestPath = function(root, target) {
+  // Initialize minimum sum to inf
+  let minSum = 0;
+  // Initialize current sum to 0
+  let currentSum = 0;
+  // If the root's adjacency list is empty, return 0
+  if (!root.adjList.length) return minSum;
+  // If it's adjancency list is empty and it is not equal to the target, return undefined
+  if (!root.adjList.length && root.value !== target) return -Infinity;
+  // Iterate over the root's adj list
+  for (let i = 0; i < root.adjList.length; i += 1) {
+    const currentNode = root.adjList[i];
+    // If the current node is the target, set minSum equal to the min of it and currentSum
+    if (currentNode === target) minSum = Math.min(minSum, currentSum + currentNode.value);
+    // Else find the minimum sum from the current node
+    else minSum = Math.min(minSum, currentSum + shortestPath(currentNode, target));
+  }
+  // Return the minimum sum, if we dont find a path, keep it at 0
+  return minSum;
+}
+
+// Tests:
+const root = new Node(2);
+const second = new Node(6);
+const third = new Node(2);
+const fourth = new Node(4);
+const fifth = new Node(1);
+root.adjList.push(second);
+root.adjList.push(third);
+second.adjList.push(fourth);
+third.adjList.push(fourth);
+fourth.adjList.push(fifth);
+console.log(shortestPath(root, fifth));
