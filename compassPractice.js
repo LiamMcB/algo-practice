7effd0aa5bc3e296eebffbf696a7fc352872c730
liameno16 <liameno16@gmail.com>
2021-01-15 10:05:18 -0800

    finished reverse int with string and alternate constant space solution

--- compassPractice.js
+++ compassPractice.js
@@ -22,3 +22,43 @@ const productArray = function(arr) {
 // Tests:
 const arr1 = [1, 2, 3, 4, 5];
 console.log(productArray(arr1));
+
+/* 3. Write a function to reverse an integer, without using any intermediate storage except for other integer values. */
+const reverseInt = function(num) {
+  // Convert integer to string
+  const intString = num.toString(10);
+  // Get multiplier based on length of string
+  let multiplier = 10 ** (intString.length - 1);
+  // Get last char of int string
+  let currentChar = intString.length - 1;
+  let output = 0;
+  // Iterate until currentChar is 0
+  while (currentChar >= 0) {
+    let currentNumber = Number(intString[currentChar]);
+    // Multiply current number by multiplier and add to output
+    output += currentNumber * multiplier;
+    // Divide multiplier by ten and decrement current char
+    currentChar -= 1;
+    multiplier /= 10;
+  }
+  return output;
+}
+// Solution without using strings
+const reverseAlt = function(num) {
+  // Initialize reversed number to 0
+  let reversed = 0;
+  // Iterate until num is 0
+  while (num !== 0) {
+    // Get last digit of number
+    let lastDigit = num % 10;
+    // Pop last digit off of num param
+    num = Math.floor(num / 10);
+    // Get reversed by multiplying current reversed by 10 and adding last digit
+    reversed = (reversed * 10) + lastDigit;
+    if (reversed < Math.pow(-2, 31) || reversed > Math.pow(2, 31) - 1) return -1; // Prevent integer overflow
+  }
+  return reversed;
+}
+// Tests:
+// 12 -> 21, 435 -> 534
+console.log(reverseAlt(435689129))
