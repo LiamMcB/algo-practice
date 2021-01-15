02e33f43edef0dbe369c9f59186918201ab27f04
liameno16 <liameno16@gmail.com>
2021-01-15 09:32:32 -0800

    finally got this one, really tough to wrap my head around how to convert from decimal to another base

--- convertToBase.js
+++ convertToBase.js
@@ -23,11 +23,20 @@ const convertBase = function(num, original, other) {
 }
 
 const convertFromBase10 = function(num, base) {
-  // Sort of cheating, but converting to arbitrary base is proving to be more work than its worth
-  return Number(num.toString(base));
+  let convertedVal = 0;
+  let multiplier = 1;
+  // Iterate until param num is 0
+  while (num != 0) {
+    let remainder = num % base;
+    num = Math.floor(num / base);
+    convertedVal += remainder * multiplier;
+    multiplier *= 10;
+  }
+  return convertedVal;
 }
 
 // Tests:
 // Ex: 112 in base 4 is 22 in base 10, 12 in base 6 (8) is 1000 in base 2, 112 in base 4 is 10110 in binary
 // console.log(convertBase(112, 4, 10));
 console.log(convertBase(112, 4, 2));
+console.log(convertBase(101101, 3, 7));
